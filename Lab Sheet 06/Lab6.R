1e4576a1c5aa9522784c4e8c2b2b92410c3264c9
Poorna Rajapakshe <it24100356@my.sliit.lk>
2025-09-02 20:38:20 +0530

    R

---
+++ Lab Sheet 06/Lab6.R
@@ -0,0 +1,16 @@
+getwd()
+setwd("//Users//poornarajapakshe//Documents//R")
+
+#1
+#I- n = 50, p = 0.85
+
+#II
+pbinom(47, 50, 0.85)
+
+#2
+#I - number of calls per hour
+
+#II - Poisson distribution -> lambda = 12
+
+#III
+dpois(15, 5)
